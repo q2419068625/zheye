747b21aedb251aa712c0300161ca2909c3e7c2a6
q2419068625 <2419068625@qq.com>
2021-02-06 22:43:43 +0800

    ValidateInput 构建及验证规则

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,69 @@
 <template>
-<div class="container">
-  <gobal-header :user="user"></gobal-header>
-  <column-list :list="list"></column-list>
-</div>
+  <div class="container">
+    <gobal-header :user="user"></gobal-header>
+    <!-- <column-list :list="list"></column-list> -->
+    <form>
+      <div class="mb-3">
+        <label class="form-label">邮箱地址:</label>
+        <validate-input :rules="emailRules"></validate-input>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱地址:</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          v-model="emailRef.val"
+          @blur="validateEmail"
+        />
+        <div id="emailHelp" v-if="emailRef.error" class="form-text">
+          {{ emailRef.message }}
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密码</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+      <div class="mb-3 form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, reactive } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
-const testData:ColumnProps[] = [
+import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
+const testData: ColumnProps[] = [
   {
     id: 1,
     title: 'test1的专栏',
     description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+    avatar:
+      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
   },
   {
     id: 2,
     title: 'test2的专栏',
     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+    avatar:
+      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
   },
   {
     id: 3,
     title: 'test2的专栏',
     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+    avatar:
+      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
   },
   {
     id: 4,
@@ -38,29 +75,52 @@ const testData:ColumnProps[] = [
     id: 5,
     title: 'test2的专栏',
     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  }
+    avatar:
+      'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100',
+  },
 ]
-import GobalHeader, {UserProps} from './components/GobalHeader.vue'
-const testUser: UserProps ={
+import GobalHeader, { UserProps } from './components/GobalHeader.vue'
+const testUser: UserProps = {
   isLogin: true,
-  name: 'lisi'
+  name: 'lisi',
 }
+const emailReg = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/
 export default defineComponent({
   name: 'App',
   components: {
     ColumnList,
-    GobalHeader
+    GobalHeader,
+    ValidateInput
   },
   setup() {
+    const emailRules: RulesProp = [
+      {type: 'required', message: '电子邮箱地址不能为空'},
+      {type: 'email', message: '请输入正确的电子邮箱格式'},
+    ]
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: ''
+    })
+    const validateEmail = () => {
+      if(emailRef.val.trim() === '') {
+        emailRef.error = true;
+        emailRef.message = 'can not be empty'
+      }else if(!emailReg.test(emailRef.val)) {
+        emailRef.error = true;
+        emailRef.message = 'should be valid email'
+      }
+    }
+
     return {
       list: testData,
-      user: testUser
+      user: testUser,
+      emailRef,
+      validateEmail,
+      emailRules
     }
-  }
-});
+  },
+})
 </script>
 
-<style>
-
-</style>
+<style></style>
